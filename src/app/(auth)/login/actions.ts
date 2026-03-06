b61218468b7080d23f842e3d6d4f7d72c23fff90
kouya0911 <kouyaosa0911@gmail.com>
2026-03-06 20:42:17 +0900

    chage

--- src/app/(auth)/login/actions.ts
+++ src/app/(auth)/login/actions.ts
@@ -9,7 +9,7 @@ export async function login() {
     const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-        redirectTo: 'http://localhost:3000//auth/callback',
+        redirectTo: `${location.origin}/auth/callback`,
   },
 })
   if (error) {
